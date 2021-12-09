e78e8d4825463ca38a557d83e86ddda9a0108f2c
Wouter de Jong <wouter@wouterj.nl>
2021-12-09 15:23:35 +0100

    Add login attempt logger

---
+++ src/EventListener/LoginAttemptListener.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\EventListener;
+
+use Psr\Log\LoggerInterface;
+use Symfony\Component\EventDispatcher\Attribute\AsEventListener;
+use Symfony\Component\Security\Http\Authenticator\Passport\Badge\UserBadge;
+use Symfony\Component\Security\Http\Event\LoginFailureEvent;
+use Symfony\Component\Security\Http\Event\LoginSuccessEvent;
+
+class LoginAttemptListener
+{
+    private LoggerInterface $logger;
+
+    public function __construct(LoggerInterface $logger)
+    {
+        $this->logger = $logger;
+    }
+
+    #[AsEventListener(event: LoginSuccessEvent::class)]
+    #[AsEventListener(event: LoginFailureEvent::class)]
+    public function onLoginAttempt(LoginSuccessEvent|LoginFailureEvent $event)
+    {
+        $userIdentifier = $event->getPassport()->getBadge(UserBadge::class)->getUserIdentifier();
+        $clientIp = $event->getRequest()->getClientIp();
+        $success = $event instanceof LoginSuccessEvent;
+
+        $this->logger->info('Login attempt for "{user}"', [
+            'user' => $userIdentifier,
+            'ip' => $clientIp,
+            'success' => $success,
+            'error' => !$success ? $event->getException()->getMessage() : null,
+        ]);
+    }
+}
