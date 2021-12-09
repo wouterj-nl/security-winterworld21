d5e3437a9b399b90aa6b059145ba70edf5101abe
Wouter de Jong <wouter@wouterj.nl>
2021-12-09 15:28:50 +0100

    Add the api firewall with json login

--- src/Controller/ApiLoginController.php
+++ src/Controller/ApiLoginController.php
@@ -2,18 +2,25 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Attribute\CurrentUser;
 
 class ApiLoginController extends AbstractController
 {
     #[Route('/api/login', name: 'api_login')]
-    public function index(): Response
+    public function index(#[CurrentUser] ?User $user): Response
     {
+        if (null === $user) {
+            return $this->json([
+                'message' => 'missing credentials',
+            ], Response::HTTP_UNAUTHORIZED);
+        }
+
         return $this->json([
-            'message' => 'Welcome to your new controller!',
-            'path' => 'src/Controller/ApiLoginController.php',
+            'token' => 'verySecureToken',
         ]);
     }
 }
