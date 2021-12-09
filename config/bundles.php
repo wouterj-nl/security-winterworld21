7965f6bbdf79e662a251ec39b54c94e1b4eee4ba
Wouter de Jong <wouter@wouterj.nl>
2021-12-09 15:01:10 +0100

    $ composer require symfonycasts/verify-email-bundle

--- config/bundles.php
+++ config/bundles.php
@@ -9,4 +9,5 @@ return [
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
+    SymfonyCasts\Bundle\VerifyEmail\SymfonyCastsVerifyEmailBundle::class => ['all' => true],
 ];
